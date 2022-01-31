71961064da00419fdad235548960b6e3404acb23
daniel-ljung-nti-johanneberg <daniel.ljung@elev.ga.ntig.se>
2022-01-31 17:03:18 +0100

    grunden

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+
+const cards = 12;
+
+function Game() {
+
+    console.log("Game started")
+
+
+    // Generate cards
+    
+    let element = document.createElement("img");
+
+
+    
+
+
+
+
+}
+
+Game()
