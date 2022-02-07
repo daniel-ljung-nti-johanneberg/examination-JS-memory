a5c16cb0f43eea152b090cdb339c744d7947604f
daniel-ljung-nti-johanneberg <daniel.ljung@elev.ga.ntig.se>
2022-02-07 17:25:23 +0100

    start

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,57 @@
+function shuffle(cards) {
 
-const cards = 12;
+    new_cards = cards.sort(() => Math.random() - 0.5)
+
+    return new_cards
+}
+
+function generatecards() {
+    let cards = [];
+
+    for (let i = 1; i <= 6; i++) {
+        
+        let currentImg = `img/char-${i}.png`;
+        cards.push(currentImg);
+
+        if (cards.length == 6) {
+            i = 0
+        } 
+
+    }
+
+    shuffle(cards)
+    return cards
+}
 
 function Game() {
 
+  
     console.log("Game started")
 
+    let active = [];
+    let cards = generatecards()
 
-    // Generate cards
+    // Render cards
+
+    for (let i = 1; i <= 12; i++) {
+
+        const field = document.getElementById("cards");
+        
+        const imgcontainer = document.createElement("div");
+        const img = document.createElement("img");
+        img.src = cards[i - 1];
+
+        field.appendChild(imgcontainer)
+        imgcontainer.appendChild(img)
+        img.classList.add("disabled");
+
+        imgcontainer.addEventListener("click", turncard => {
+            img.classList.remove("disabled");
     
-    let element = document.createElement("img");
+        })
 
 
-    
-
-
+    }
 
 
 }
