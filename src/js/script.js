d9acfa29399f219af191784e610d55a5833f732f
daniel-ljung-nti-johanneberg <daniel.ljung@elev.ga.ntig.se>
2022-02-21 17:59:18 +0100

    fixat if-statement, lagt till disabled

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,5 @@
 function Shuffle(cards) {
-
     new_cards = cards.sort(() => Math.random() - 0.5)
-
     return new_cards
 }
 
@@ -29,30 +27,29 @@ function generateCards() {
 
 function Game() {
 
-  
     console.log("Game started")
-
     let active = [];
     let activeIndex = [];
     let cards = generateCards()
-''
+    let disabled = false
+
+
     // Render cards
 
     for (let i = 1; i <= 24; i++) {
 
         const field = document.getElementById("cards");
-        
         const imgcontainer = document.createElement("div");
-        const img = document.createElement("img");
-        img.src = cards[i - 1];
+        const img = document.createElement("img")
 
+        img.src = cards[i - 1];
         field.appendChild(imgcontainer)
         imgcontainer.appendChild(img)
         img.classList.add("disabled");
 
         imgcontainer.addEventListener("click", turncard => {
 
-            if (active.length <= 2) {
+            if (active.length < 2 && disabled == false) {
 
                 img.classList.remove("disabled");
                 active.push(cards[i-1])
@@ -60,40 +57,41 @@ function Game() {
 
             }
 
-            if (active.length == 2) {
+            if (active.length == 2 && disabled == false) {
 
                 if (active[0] == active[1]) {
+
                     console.log("match")
                     active = [];
                     activeIndex = [];
+
                 }
 
-                if (active[0] != active[1] ) {
+                if (active[0] != active[1]) {
+
+                    disabled = true
 
                     console.log(activeIndex)
 
-          
                     document.querySelector(`#cards div:nth-child(${activeIndex[0]}) img`).classList.remove("disabled");
                     document.querySelector(`#cards div:nth-child(${activeIndex[1]}) img`).classList.remove("disabled");
-
 
                     setTimeout(() => {
                         document.querySelector(`#cards div:nth-child(${activeIndex[0]}) img`).classList.add("disabled");
                         document.querySelector(`#cards div:nth-child(${activeIndex[1]}) img`).classList.add("disabled");
-                        
                         activeIndex = [];
                         active = [];
+                        disabled = false
                     }, 1000)
+
+
                 }
 
             }
-      
-    
+
         })
 
-
     }
-
 
 }
 
